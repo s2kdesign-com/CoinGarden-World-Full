5d79b6d0610aab1d9cb5583a0b27098dcded624f
Svetlin Krastanov <svetlin.krastanov90@gmail.com>
2022-09-16 19:40:43 +0300

    #15 Added ExternalAuthState provider for the MAUI mobile applications

---
+++ src/mobile-apps/CoinGardenWorld.Maui/Authorization/ExternalAuthStateProvider.cs
@@ -0,0 +1,49 @@
+﻿using Microsoft.AspNetCore.Components.Authorization;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Claims;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CoinGardenWorld.Maui.Authorization
+{
+    public class ExternalAuthStateProvider : AuthenticationStateProvider
+    {
+        private AuthenticationState currentUser;
+
+        public ExternalAuthStateProvider(ExternalAuthService service)
+        {
+            currentUser = new AuthenticationState(service.CurrentUser);
+
+            service.UserChanged += (newUser) =>
+            {
+                currentUser = new AuthenticationState(newUser);
+                NotifyAuthenticationStateChanged(Task.FromResult(currentUser));
+            };
+        }
+
+        public override Task<AuthenticationState> GetAuthenticationStateAsync() =>
+            Task.FromResult(currentUser);
+    }
+
+    public class ExternalAuthService
+    {
+        public event Action<ClaimsPrincipal>? UserChanged;
+        private ClaimsPrincipal? currentUser;
+
+        public ClaimsPrincipal CurrentUser
+        {
+            get { return currentUser ?? new(); }
+            set
+            {
+                currentUser = value;
+
+                if (UserChanged is not null)
+                {
+                    UserChanged(currentUser);
+                }
+            }
+        }
+    }
+}
--- src/mobile-apps/CoinGardenWorld.Maui/MauiProgram.cs
+++ src/mobile-apps/CoinGardenWorld.Maui/MauiProgram.cs
@@ -1,5 +1,8 @@
-﻿using CoinGardenWorld.Maui.Data;
+﻿using CoinGardenWorld.Maui.Authorization;
+using CoinGardenWorld.Maui.Data;
+using Microsoft.AspNetCore.Components.Authorization;
 using Microsoft.AspNetCore.Components.WebView.Maui;
+using Microsoft.Extensions.Configuration;
 
 namespace CoinGardenWorld.Maui
 {
@@ -15,10 +18,28 @@ namespace CoinGardenWorld.Maui
                     fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
                 });
 
+            using var stream = FileSystem.OpenAppPackageFileAsync("appsettings.json").Result;
+
+            var config = new ConfigurationBuilder()
+                .AddJsonStream(stream)
+                .Build();
+
+            builder.Configuration.AddConfiguration(config);
+
+            // Add authorization (do not MSAL its not working with MAUI)
+            builder.Services.AddAuthorizationCore();
+            builder.Services.AddScoped<AuthenticationStateProvider, ExternalAuthStateProvider>();
+            builder.Services.AddSingleton<ExternalAuthService>();
+            //builder.Services.AddMsalAuthentication(options =>
+            //{
+            //    builder.Configuration.Bind("AzureAd", options.ProviderOptions.Authentication);
+            //});
+
             builder.Services.AddMauiBlazorWebView();
 #if DEBUG
 		builder.Services.AddBlazorWebViewDeveloperTools();
 #endif
+
 
             // Add CoinGardenWorld.MobileTheme GRPC, Pages and Components
             builder.Services.AddTheme();
